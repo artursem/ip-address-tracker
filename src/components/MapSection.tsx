e07da895b3bf80ada0e04cb7aa637c2f30c27af8
artursem <artur.semeniuk@gmail.com>
2022-05-15 17:02:56 +0200

    setview on click

--- src/components/MapSection.tsx
+++ src/components/MapSection.tsx
@@ -1,12 +1,26 @@
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvent } from 'react-leaflet';
+import { Icon } from 'leaflet';
 import styles from './styles/MapSection.module.css';
+import { Coords } from '../types/apiResponse';
 
 interface MapSectionProps {
-	coords: [number, number];
+	coords: Coords;
 	popup?: string;
 }
 
+const blackMarker = new Icon({
+	iconUrl: '/icon-location.svg',
+	iconSize: [35, 45],
+});
+
 const MapSection = ({ coords, popup }: MapSectionProps) => {
+	function SetViewOnLoad() {
+		const map = useMapEvent('click', (e) => {
+			map.setView(e.latlng, map.getZoom());
+		});
+
+		return null;
+	}
 	return (
 		<section className={styles.leafletContainer}>
 			<MapContainer center={coords} zoom={13} scrollWheelZoom={true}>
@@ -14,11 +28,12 @@ const MapSection = ({ coords, popup }: MapSectionProps) => {
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 				/>
-				<Marker position={coords}>
+				<Marker position={coords} icon={blackMarker}>
 					<Popup>
 						A pretty CSS3 popup. <br /> Easily customizable.
 					</Popup>
 				</Marker>
+				<SetViewOnLoad />
 			</MapContainer>
 		</section>
 	);
